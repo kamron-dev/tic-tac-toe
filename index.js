010e119b8ffb616b1ec2973da8d3696cf2c5166b
kamron-dev <k.kamron@yadnex.ru>
2023-07-14 00:29:01 +0500

    endGame works clearing the gameBoard array, buttons, removing the disabled attribute, defaulting the current player to player1, and defaulting the display

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ const renderGame = (() => {
     const gameBoardDiv = document.querySelector("#gameBoardDiv");
     const display = document.querySelector("#display");
     let currentPlayer = player1;
-
+    const restartGameBtn = document.querySelector("#restartGame")
+    
+    
     // functions to use inside of the game "switchPLayer" and "checkWin"
     const switchPlayer = () => {
         currentPlayer === player1 ? currentPlayer = player2 : currentPlayer === player2 ? currentPlayer = player1 : null;
@@ -47,11 +49,10 @@ const renderGame = (() => {
     const endGame = (currentPlayer) => {
         const allButtons = document.querySelectorAll(".cell");
         allButtons.forEach(button => button.setAttribute("disabled", ""));
-        
         display.innerHTML = `${currentPlayer.name} won the game!`;
-    }
+    };
     
-    // Creating a function to render the board contents to the gameBoardDiv
+    // Creating a function to render the board content to the gameBoardDiv
     gameBoard.board.forEach((element, index) => {
         const gameButton = document.createElement("button");
         gameButton.classList.add("cell");
@@ -64,14 +65,23 @@ const renderGame = (() => {
             gameBoard.board[index] = currentPlayer.sign;
             gameButton.setAttribute("disabled", "");
             if (checkWin(currentPlayer)) return endGame(currentPlayer);
-            if (checkDraw()) return console.log("It is a tie!");
+            if (checkDraw()) return display.innerHTML = "It is a tie!";
             switchPlayer();
             
         })
         
     });
 
-    
+    restartGameBtn.addEventListener("click", () => {
+        gameBoard.board = ["", "", "", "", "", "", "", "", ""];
+        const allButtons = document.querySelectorAll(".cell");
+        allButtons.forEach(button => {
+            button.innerHTML = "";
+            button.removeAttribute("disabled", "");
+        });
+        currentPlayer = player1;
+        display.innerHTML = "Let the best player win!";
+    });
     
 })();
 
