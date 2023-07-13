e62bab6f7765774fa6c02928e0ff3113771f8b94
kamron-dev <k.kamron@yadnex.ru>
2023-07-13 15:54:10 +0500

    added disabled to the buttons after click and started the checkWin function

--- index.js
+++ index.js
@@ -21,8 +21,18 @@ const renderGame = (() => {
     const gameBoardDiv = document.querySelector("#gameBoardDiv");
     let currentPlayer = player1;
 
+    // functions to use inside of the game "switchPLayer" and "checkWin"
     const switchPlayer = () => {
         currentPlayer === player1 ? currentPlayer = player2 : currentPlayer === player2 ? currentPlayer = player1 : null;
+    }
+
+    const checkWin = () => {
+        const winningCombinations = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], //horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], //vertical
+            [0, 4, 8], [2, 4, 6] // diagonal
+        
+        ]
     }
     
     // Creating a function to render the board contents to the gameBoardDiv
